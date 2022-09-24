5a2fbb80e0619076e9c12732a8ce50b974b47581
Calbabreaker <calbabreaker@gmail.com>
2022-09-24 16:41:44 +0930

    fix: meta tags

--- components/MetaTags.tsx
+++ components/MetaTags.tsx
@@ -13,10 +13,11 @@ export const MetaTags: React.FC<Props> = ({
     return (
         <Head>
             <title>{title}</title>
+            <meta name="description" content={description} />
             <meta name="twitter:card" content="summary" />
             <meta name="twitter:site" content="@calbabreaker" />
             <meta name="twitter:title" content={title} />
-            <meta name="twitter:description" content={description} />
+            <meta name="twitter:description" content={title} />
             {/* <meta name="twitter:image" content={image} /> */}
 
             <meta property="og:title" content={title} />
